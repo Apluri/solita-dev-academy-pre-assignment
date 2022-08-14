b6efd6e2d6e1c149f9d5679798209239d27a7a78
Apluri <henri.hietala95@gmail.com>
2022-08-14 16:41:04 +0300

    add react router, add routes to trips and station

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,9 +1,10 @@
 import { Box } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Station } from "../../../shared/dataTypes";
 import TripListView from "./TripListView";
 import StationListView from "./StationListView";
 import SingleStation from "./SingleStation";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 export const BASE_API_URL = "http://localhost:8080/api";
 export const STATION_DATA_URL = "/stationdata";
@@ -17,13 +18,33 @@ function App() {
 
   return (
     <Box sx={{ margin: "2em" }}>
-      <TripListView />
-      <Box sx={{ display: "flex", marginTop: "1em", justifyContent: "center" }}>
-        <StationListView
-          setSelectedStation={(station: Station) => setSelectedStation(station)}
+      <Routes>
+        {/**Navigate to trips by default or when user tries to access exact / path */}
+        <Route path="/" element={<Navigate to={"/trips"} />} />
+        <Route path="trips" element={<TripListView />} />
+        <Route
+          path="stations"
+          element={
+            <Box
+              sx={{
+                display: "flex",
+                marginTop: "1em",
+                justifyContent: "center",
+              }}
+            >
+              <StationListView
+                setSelectedStation={(station: Station) =>
+                  setSelectedStation(station)
+                }
+              />
+              <SingleStation
+                station={selectedStation}
+                onClose={deselectStation}
+              />
+            </Box>
+          }
         />
-        <SingleStation station={selectedStation} onClose={deselectStation} />
-      </Box>
+      </Routes>
     </Box>
   );
 }
